0fa6fa11268eab94295ec95a3a4bb434f6a5bcb5
unknown <dsml@live.com>
2024-06-23 10:04:39 -0600

    media queries

--- src/components/TimeStyles.js
+++ src/components/TimeStyles.js
@@ -4,38 +4,38 @@ export const Welcome = styled.div`
     position: absolute;
     text-align: center;
     padding: 1rem;
-    top: 50%;
+    top: 40%;
     left: 50%;
     -ms-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
 `
 
 export const TimeStamp = styled.p`
-    font-size: 8rem;
+    font-size: 4rem;
     font-weight: 500;
     margin: 0;
     padding: 0;
 
-    @media screen and (max-width: 600px) {
-        font-size: 5rem;
+    @media screen and (max-width: 640px) {
+        font-size: 3rem;
     }
-
-    @media screen and (max-width: 400px) {
-        font-size: 2.5rem;
+   
+    @media screen and (max-width: 480px) {
+        font-size: 1.5rem;
     }
 `
 
 export const Greeting = styled.p`
-    font-size: 4rem;
+    font-size: 3rem;
     font-weight: 600;
     margin: 0;
     padding: 0;
 
-    @media screen and (max-width: 600px) {
-        font-size: 3rem;
+    @media screen and (max-width: 640px) {
+        font-size: 2rem;
     }
 
-    @media screen and (max-width: 400px) {
-        font-size: 1.5rem;
+    @media screen and (max-width: 480px) {
+        font-size: 1rem;
     }
 `
